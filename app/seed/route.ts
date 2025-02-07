ed890643d9df00846be280aa6a4caac81fc24b6b
Abhishamathew1 <n01635183@humber.ca>
2025-02-07 00:39:53 -0500

    final_changes

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,8 +101,25 @@ async function seedRevenue() {
   return insertedRevenue;
 }
 
+// export async function GET() {
+//   try {
+//     const result = await sql.begin((sql) => [
+//       seedUsers(),
+//       seedCustomers(),
+//       seedInvoices(),
+//       seedRevenue(),
+//     ]);
+
+//     return Response.json({ message: 'Database seeded successfully' });
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
+
 export async function GET() {
   try {
+    await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`; // Ensure extension is created before transactions
+
     const result = await sql.begin((sql) => [
       seedUsers(),
       seedCustomers(),
@@ -115,3 +132,4 @@ export async function GET() {
     return Response.json({ error }, { status: 500 });
   }
 }
+
